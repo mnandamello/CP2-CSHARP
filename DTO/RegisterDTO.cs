1e7654f76ce8473124e722dd37cab5f44adc38b8
mnandamello <mfernanda.nintendo@gmail.com>
2024-04-16 22:32:15 -0300

    Criação do DTO

---
+++ DTO/RegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CP2_CSHARP.DTO
+{
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string UserPassword { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Compare("UserPassword", ErrorMessage = "As senhas não coincidem")]
+        public string ComparePassword { get; set; }
+
+    }
+}
