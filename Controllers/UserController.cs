a04f5d83789dee9bdeec65c11761bc4318feeeb5
Nreis280 <nicolasreissanto@gmail.com>
2024-04-17 09:34:44 -0300

    criando UserController

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CP2_CSHARP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
