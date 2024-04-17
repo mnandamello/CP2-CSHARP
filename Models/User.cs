e27e66b712b9430fbc99bc6ff6fdaa7dddc72bdb
Nreis280 <nicolasreissanto@gmail.com>
2024-04-16 22:25:22 -0300

    Criando User na Models

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,14 @@
-﻿namespace CP2_CSHARP.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CP2_CSHARP.Models
 {
-    public class User
-    {
+    public class User {
+
+        [Key]
+        public int id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string UserEmail { get; set; } = string.Empty;
+        public string UserPassword { get; set; } = string.Empty;
+
     }
 }
