04651d69b43c4add0a06f7535e17a62eb7c11e97
mnandamello <mfernanda.nintendo@gmail.com>
2024-04-16 22:15:12 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CP2_CSHARP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
